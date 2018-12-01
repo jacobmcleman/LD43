505e9f45545a5d65b98469d991d6626738ab8844
Eli <elijw22@gmail.com>
2018-12-01 16:04:39 -0600

    FloorSpikeInteractable

---
+++ Assets/Scripts/Interactables/FloorSpikes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorSpikes : Interactable {
+
+    private float fallTime = 0;
+    private bool fall = false;
+
+	public override void OnInteract(CrewMember crewMember)
+    {
+        Debug.Log("Interacted!");
+
+        crewMember.gameObject.GetComponent<Animator>().StopPlayback();
+
+        StartCoroutine("Fall", crewMember);
+        crewMember.enabled = false;
+    }
+
+    IEnumerator Fall(CrewMember crewMember)
+    {
+        Debug.Log("Fall start");
+        for (float t = 0f; t <= 1; t += 0.006f)
+        {
+            Debug.Log("Fall Time:" + t);
+            crewMember.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, -1*((1 - t) * 0 + t * 90));
+            yield return null;
+        }
+    }
+}
+
