0b06a369b387292e0a17f5d44f9ad2df6a27f6fa
jacobmcleman <j.mcleman@digipen.edu>
2018-12-01 12:12:28 -0800

    Added interactable base class

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour {
+
+    public bool roleIsRequired = true;
+    public CrewMember.Role role = CrewMember.Role.RedShirt;
+
+    public abstract void OnInteract(CrewMember crewMember);
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        CrewMember crewMember = collider.gameObject.GetComponent<CrewMember>();
+        if(crewMember != null && (!roleIsRequired || crewMember.role == role))
+        {
+            OnInteract(crewMember);
+        }
+    }
+}
---
+++ Assets/Scripts/Interactables/TestInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInteractable : Interactable {
+
+	public override void OnInteract(CrewMember crewMember)
+    {
+        Debug.Log("Interacted with by: " + crewMember.gameObject.name);
+    }
+}
