0c70ccb63c1256c2a392b547d42b04d5170a3fea
Eli <elijw22@gmail.com>
2018-12-01 22:49:53 -0600

    adding win menu and level 1

---
+++ Assets/Scripts/WinCanvas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCanvas : MonoBehaviour
+{
+    public string nextLevel;
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
