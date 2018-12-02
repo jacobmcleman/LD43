e33e93a0c50647d70e964a42a0afb7f8acc44f3e
Eli <elijw22@gmail.com>
2018-12-02 14:44:56 -0600

    Elevator animation fix

--- Assets/Scripts/Interactables/Activater.cs
+++ Assets/Scripts/Interactables/Activater.cs
@@ -14,7 +14,7 @@ public class Activater : Interactable
         if (stopCrew)
         {
             crewMember.moving = false;
-            crewMember.GetComponent<Animator>().enabled = false;
+            //crewMember.GetComponent<Animator>().enabled = false;
         }
         ToggleState();
     }
