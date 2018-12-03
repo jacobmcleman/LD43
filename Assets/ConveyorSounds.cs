cf72a7b2baa12a6002e250c2cea7dad58acc7d51
cmeese549 <chris@meesewebdesigns.com>
2018-12-03 15:56:04 -0700

    Conveyors and bumping and mixing updates and menu sounds but not actually

---
+++ Assets/ConveyorSounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorSounds : MonoBehaviour {
+
+	AudioSource conveyorScreamer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		conveyorScreamer = GetComponent<AudioSource>();	
+		StartCoroutine(loop());
+	}
+	
+	IEnumerator loop ()
+	{
+		yield return new WaitForSecondsRealtime(5);
+		conveyorScreamer.Play();
+	}
+}
